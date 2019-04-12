a3c3a04693b90a3cd3b6490d9a6dff813b6a96af
Wanjiku <Wanjiku>
2019-04-12 10:33:42 +0300

    Progress commit

--- app/request.py
+++ app/request.py
@@ -1,7 +1,75 @@
 from app import app
+import urllib.request,json #will help us create a connection to our API URL, send a request, json will format response to a python dictionary
+from .models import movie
 
-# Getting api key
-api_key = app.config['MOVIE_API_KEY']from app import app
+Movie = movie.Movie
 
 # Getting api key
 api_key = app.config['MOVIE_API_KEY']
+
+# Getting the movie base url
+base_url = app.config["MOVIE_API_BASE_URL"]
+
+def get_movies(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_movies_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_movies_url) as url:
+        get_movies_data = url.read() #reads response from the url request from api request
+        get_movies_response = json.loads(get_movies_data) #convert json data to a python dictionary 
+
+        movie_results = None
+
+        if get_movies_response['results']:
+            movie_results_list = get_movies_response['results'] #checks to see if the response has any data
+            movie_results = process_results(movie_results_list) #if it does we call a process_results() which takes in the list of dict objects and returns a list of movie objects
+    return movie_results #returns a list of movie objects
+# function that will process the results and create movie objects from the elements we need
+
+def process_results(movie_list):
+    '''
+    Function  that processes the movie result and transform them to a list of Objects
+
+    Args:
+        movie_list: A list of dictionaries that contain movie details
+
+    Returns :
+        movie_results: A list of movie objects
+    '''
+    movie_results = []
+    for movie_item in movie_list:
+        id = movie_item.get('id')
+        title = movie_item.get('original_title')
+        overview = movie_item.get('overview')
+        poster = movie_item.get('poster_path')
+        vote_average = movie_item.get('vote_average')
+        vote_count = movie_item.get('vote_count')
+
+        if poster:
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+            movie_results.append(movie_object)
+
+    return movie_results #returns a list of movie objects
+
+
+def get_movie(id):
+    get_movie_details_url = base_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_movie_details_url) as url:
+        movie_details_data = url.read()
+        movie_details_response = json.loads(movie_details_data)
+
+        movie_object = None
+        if movie_details_response:
+            id = movie_details_response.get('id')
+            title = movie_details_response.get('original_title')
+            overview = movie_details_response.get('overview')
+            poster = movie_details_response.get('poster_path')
+            vote_average = movie_details_response.get('vote_average')
+            vote_count = movie_details_response.get('vote_count')
+
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+
+    return movie_object
